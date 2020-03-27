a1e03281c6290c310c21f84024be454bd55210f9
Oluwatosin123 <tosinemidastouch@gmail.com>
2020-03-27 06:34:15 +0100

    2file added to my js task repository

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = "Tosin Ajose";
+let courses = ["html", "css", "javascript", "design"];
+console.log("My name is:  " + name + " and my courses are " + courses);
+
+const courseNumberIsEven = [];
+
+for (let i = 1; i <= 200; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
