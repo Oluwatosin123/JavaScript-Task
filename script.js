32e2cedd59a563d6d5dbc3cbd2b8db650569ff5f
Oluwatosin123 <tosinemidastouch@gmail.com>
2020-03-27 08:02:27 +0100

    newly modified files

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let name = "Tosin Ajose";
+var name = "Tosin Ajose";
 let courses = ["html", "css", "javascript", "design"];
 console.log("My name is:  " + name + " and my courses are " + courses);
 
-const courseNumberIsEven = [];
-
+const courseNumberEven = [];
 for (let i = 1; i <= 200; i++) {
   if (i % 2 === 0) {
     console.log(i);
